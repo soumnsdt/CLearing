b39fcb6027fccde3b469845172f294824e9551e5
soumnsdt <949761196@qq.com>
2018-01-07 00:36:40 +0800

    【1】当用户输入的值不合法时，要求重新输入

--- CLearing/hotal.c
+++ CLearing/hotal.c
@@ -8,15 +8,29 @@ char hotelName[4][50] = {
 	"布丁酒店","南京酒店","常州酒店","金坛酒店"
 };
 
-int menu()
+int menu(void)
 {
-	int choise = 0;
+	int choise = 0;//用户的选择
+	int result; //用户输入后的结果判断  0：表示非法输入   1：表示合法
 	printf("请选择您需要入住的酒店：\n");
 	for (int i = 0; i < 4; i++)
 	{
 		printf("%d、%s\n", i + 1, hotelName[i]);
 	}
 	printf("请输入您的选择：");
+
+	//【1】当用户输入的值不合法时，要求重新输入
+	while ((result = scanf_s("%d",&choise))!=1 || choise < 1 || choise>5)
+	{
+		if (result != 1)//如果用户输入的不是数字类型
+		{
+			scanf_s("%*s");//用来处理非整数的输入。*表示输入项在读入后不赋值给对应的变量
+			fflush(stdin); //清空缓冲区
+		}
+		printf("必须输入1-4之间的整数，请重新输入：");
+	}
 	scanf_s("%d", &choise);
+	
+
 	return choise;
 }
--- CLearing/main.c
+++ CLearing/main.c
@@ -4,14 +4,13 @@
 #include"hotal.h"
 
 //访问外部文件
-//
 extern char hotelName[4][50];
 
 int main()
 {
 
 	int choise;
-	//用户输入酒店和天数，程序计算出总费用。
+	//【要实现的功能：】用户输入酒店和天数，程序计算出总费用。
 	//菜单函数
 	choise = menu();
 	if (choise > 0 && choise < 5)

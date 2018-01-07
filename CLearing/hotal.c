375f69424bec09a75ebeb11f7dcaf05e679dd811
soumnsdt <949761196@qq.com>
2018-01-07 13:35:19 +0800

    修改了输入非数字之后，需要输入两次数字才能显示您选择的酒店名字

--- CLearing/hotal.c
+++ CLearing/hotal.c
@@ -17,19 +17,20 @@ int Menu(void)
 	{
 		printf("%d、%s\n", i + 1, hotelName[i]);
 	}
-	printf("5、退出程序。\n");
+	printf("5、退出程序\n");
 	printf("请输入您的选择：");
+	result = scanf_s("%d", &choice);
 
 	//【1】当用户输入的值不合法时，要求重新输入
-	while ((result = scanf_s("%d", &choice)) != 1 || choice < 1 || choice > 5)
+	while (result != 1 || choice < 1 || choice > 5)
 	{
 		if (result != 1)//如果用户输入的不是数字类型
 		{
-			//scanf_s("%*s");//用来处理非整数的输入。*表示输入项在读入后不赋值给对应的变量
-			fflush(stdin); //清空缓冲区
+			scanf_s("%*s");//用来处理非整数的输入。*表示输入项在读入后不赋值给对应的变量
+			//fflush(stdin); //清空缓冲区
 		}
 		printf("必须输入1-5之间的整数，请重新输入：");
-		scanf_s("%d", &choice);
+		result = scanf_s("%d", &choice);
 	}
 
 	return choice;

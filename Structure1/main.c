671d258c294cb36563beb102a88bb9a5d59fece8
soumnsdt <949761196@qq.com>
2018-01-11 11:44:46 +0800

    结构指针的两种使用方法

--- Structure1/main.c
+++ Structure1/main.c
@@ -10,3 +10,41 @@
 				(2)、结构指针变量 -> 成员变量名
 
 */
+#include<stdio.h>
+#include<Windows.h>
+
+//门派
+struct Martial
+{
+	int id;           //门派id
+	char name[50];    //门派的名称
+	int count;        //门派人数
+	int type;         //门派的类型 1-正派 2-中立 3-邪派
+};
+
+//玩家
+struct Player
+{
+	int id;
+	char name[50];
+	int password;		//玩家的登入密码    
+						//int password[50]:int 数组不像char数组，他是接收数据的，每个元素的空间都是int类型的最大值
+	char sex[50];				//玩家性别
+	struct Martial martial;
+};
+
+
+int main()
+{
+	struct Player player = { 1,"不知火舞",12345,"nan",{2,"lol",500,3} };
+	struct Player * ptr_player1 = &player;
+	//访问结构成员的一般形式：
+	//【1】
+	printf("%s\t%s\n", (*ptr_player1).name, (*ptr_player1).martial.name);
+	//【2】
+	printf("%s\t%s\n", ptr_player1->name, ptr_player1->martial.name);
+
+
+	system("pause");
+	return 0;
+}
